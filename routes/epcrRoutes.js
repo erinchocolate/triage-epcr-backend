ccdb91b19c7da5b762c0de6ddfaf672ec411f564
Meiqiao <chenmeiqiao940926@gmail.com>
2023-02-13 10:28:56 +1300

    change location_note to incident_location

--- routes/epcrRoutes.js
+++ routes/epcrRoutes.js
@@ -15,7 +15,7 @@ router.get("/", (req, res) => {
 // @route POST - /epcrs/
 // @desc Create a new EPCR
 router.post("/", (req, res) => {
-  const sql = `INSERT INTO records (first_name, middle_name, last_name, nhi_number, dob, gender, age, address, patient_medication, patient_allergy, incident_type, incident_note, notified_time, responded_time, located_time, departed_time, destination_time, location_note, subjective_note, objective_note, assessment_note, plan_note, vehicle, transport_status, destination, estimate_arrival_time, incident_medication, cardioversion, pacing, cardiac_arrest, rsi, mechanical_ventilation, cpap, sugical_cric, needle_decompression, finger_thoracostomy, fi_block, bp, heart_rate, resp_rate, temp, bsl, spo2, etco2, monitor, manual, palpatation, eye_response, voice_response, motor_response, four_lead_ecg, twelve_lead_ecg, opa, opa_size, opa_location, lma, lma_size, lma_location,npa, npa_size, npa_location, ett, ett_size, ett_location, peep, peep_note, suction, suction_catheter, bvm) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+  const sql = `INSERT INTO records (first_name, middle_name, last_name, nhi_number, dob, gender, age, address, patient_medication, patient_allergy, incident_type, incident_note, notified_time, responded_time, located_time, departed_time, destination_time, incident_location, subjective_note, objective_note, assessment_note, plan_note, vehicle, transport_status, destination, estimate_arrival_time, incident_medication, cardioversion, pacing, cardiac_arrest, rsi, mechanical_ventilation, cpap, sugical_cric, needle_decompression, finger_thoracostomy, fi_block, bp, heart_rate, resp_rate, temp, bsl, spo2, etco2, monitor, manual, palpatation, eye_response, voice_response, motor_response, four_lead_ecg, twelve_lead_ecg, opa, opa_size, opa_location, lma, lma_size, lma_location,npa, npa_size, npa_location, ett, ett_size, ett_location, peep, peep_note, suction, suction_catheter, bvm) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
   const patientData = [
     req.body.first_name,
     req.body.middle_name,
@@ -34,7 +34,7 @@ router.post("/", (req, res) => {
     req.body.located_time,
     req.body.departed_time,
     req.body.destination_time,
-    req.body.location_note,
+    req.body.incident_location,
     req.body.subjective_note,
     req.body.objective_note,
     req.body.assessment_note,
@@ -128,7 +128,7 @@ router.put("/:id", (req, res) => {
     req.body.located_time,
     req.body.departed_time,
     req.body.destination_time,
-    req.body.location_note,
+    req.body.incident_location,
     req.body.subjective_note,
     req.body.objective_note,
     req.body.assessment_note,
@@ -181,7 +181,7 @@ router.put("/:id", (req, res) => {
     req.body.suction_catheter,
     req.body.bvm,
   ];
-  const sql = `UPDATE records SET first_name = ?, middle_name = ?, last_name = ?, nhi_number = ?, dob = ?, gender = ?, age = ?, address = ?, patient_medication = ?, patient_allergy = ?, incident_type = ?, incident_note = ?, notified_time = ?, responded_time = ?, located_time = ?, departed_time = ?, destination_time = ?, location_note = ?, subjective_note = ?, objective_note = ?, assessment_note = ?, plan_note = ?, vehicle = ?, transport_status = ?, destination = ?, estimate_arrival_time = ?, incident_medication = ?, cardioversion = ?, pacing = ?, cardiac_arrest = ?, rsi = ?, mechanical_ventilation = ?, cpap = ?, sugical_cric = ?, needle_decompression = ?, finger_thoracostomy = ?, fi_block = ?,bp= ?, heart_rate= ?, resp_rate= ?, temp= ?, bsl= ?, spo2= ?, etco2= ?, monitor= ?, manual= ?, palpatation= ?, eye_response= ?, voice_response= ?, motor_response= ?, four_lead_ecg= ?, twelve_lead_ecg= ?, opa= ?, opa_size= ?, opa_location= ?, lma= ?, lma_size= ?, lma_location= ?,npa= ?, npa_size= ?, npa_location= ?, ett= ?, ett_size= ?, ett_location= ?, peep= ?, peep_note= ?, suction= ?, suction_catheter= ?, bvm= ? WHERE incident_id = ${id}`;
+  const sql = `UPDATE records SET first_name = ?, middle_name = ?, last_name = ?, nhi_number = ?, dob = ?, gender = ?, age = ?, address = ?, patient_medication = ?, patient_allergy = ?, incident_type = ?, incident_note = ?, notified_time = ?, responded_time = ?, located_time = ?, departed_time = ?, destination_time = ?, incident_location = ?, subjective_note = ?, objective_note = ?, assessment_note = ?, plan_note = ?, vehicle = ?, transport_status = ?, destination = ?, estimate_arrival_time = ?, incident_medication = ?, cardioversion = ?, pacing = ?, cardiac_arrest = ?, rsi = ?, mechanical_ventilation = ?, cpap = ?, sugical_cric = ?, needle_decompression = ?, finger_thoracostomy = ?, fi_block = ?,bp= ?, heart_rate= ?, resp_rate= ?, temp= ?, bsl= ?, spo2= ?, etco2= ?, monitor= ?, manual= ?, palpatation= ?, eye_response= ?, voice_response= ?, motor_response= ?, four_lead_ecg= ?, twelve_lead_ecg= ?, opa= ?, opa_size= ?, opa_location= ?, lma= ?, lma_size= ?, lma_location= ?,npa= ?, npa_size= ?, npa_location= ?, ett= ?, ett_size= ?, ett_location= ?, peep= ?, peep_note= ?, suction= ?, suction_catheter= ?, bvm= ? WHERE incident_id = ${id}`;
   db.query(sql, patientData, (err, result) => {
     if (err) return res.json(err);
     return res.json(result);
